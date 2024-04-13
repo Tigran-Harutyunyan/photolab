d9a17e1593e618c4ea842e41d3a8df21565aa44f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:44:32 +0400

    created the default layout that will fetch categories from Strapi too

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import type { TCategory } from "@/types";
+
+import { useMain } from "@/store/main";
+
+const { setCategories } = useMain();
+
+const { find } = useStrapi();
+
+const { data } = await find("categories");
+
+if (data && Array.isArray(data)) {
+  setCategories(data as TCategory[]);
+}
+</script>
+
+<template>
+  <div>
+    <Header />
+    <slot />
+    <Footer />
+  </div>
+</template>
