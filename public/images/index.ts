c20559da6712cb67badb74511a37b8dd81b9cffc
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:42:38 +0400

    added assets and public folders

---
+++ public/images/index.ts
@@ -0,0 +1,6 @@
+import Camera from './camera.png';
+import MainSlider from './mainSlider_bg.png';
+import PromoImg1 from './promo_img1.png';
+import PromoImg2 from './promo_img2.png';
+
+export { PromoImg1, PromoImg2, Camera, MainSlider };
