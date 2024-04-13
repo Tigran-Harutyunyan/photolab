c2933d880888ad5ffd9c9c6ad909a2572f6aea61
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:42:56 +0400

    created icon files

---
+++ components/ui/icons/IoArrowForward.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    stroke-width="0"
+    viewBox="0 0 512 512"
+    class="text-lg"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="48"
+      d="M268 112l144 144-144 144m124-144H100"
+    ></path>
+  </svg>
+</template>
