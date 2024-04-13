78a51de95b86870752beaa353ce86c16687ab4a7
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:44:57 +0400

    created Header and Footer components

---
+++ components/Footer.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import FaFacebook from "@/components/ui/icons/FaFacebook.vue";
+import FaInstagram from "@/components/ui/icons/FaInstagram.vue";
+import FaTwitter from "@/components/ui/icons/FaTwitter.vue";
+import FaYoutube from "@/components/ui/icons/FaYoutube.vue";
+</script>
+
+<template>
+  <footer class="pt-16 bg-primary">
+    <div class="container mx-auto">
+      <div class="text-center">
+        <h2 class="mb-6 font-semibold uppercase h2">
+          Subscribe to our newsletter
+        </h2>
+        <p class="text-white/70">
+          Be the first to get the latest news about trends, promotions and much
+          more!
+        </p>
+      </div>
+
+      <form
+        class="flex flex-col gap-5 mx-auto my-14 w-full max-w-3xl sm:flex-row"
+      >
+        <input class="input" placeholder="Your email address" type="text" />
+        <button class="btn btn-accent min-w-[150px]" type="button">Join</button>
+      </form>
+
+      <div
+        class="flex gap-x-6 mx-auto mb-9 max-w-max text-base capitalize text-white/60"
+      >
+        <a class="transition-all hover-text-white" href="#"> Returns Policy </a>
+        <a class="transition-all hover-text-white" href="#">
+          Track Your Order
+        </a>
+        <a class="transition-all hover-text-white" href="#">
+          Shipping & delivery
+        </a>
+      </div>
+
+      <div class="flex gap-x-6 mx-auto mb-16 max-w-max text-lg">
+        <a class="transition-all hover-text-white" href="#">
+          <FaYoutube />
+        </a>
+        <a class="transition-all hover-text-white" href="#">
+          <FaInstagram />
+        </a>
+        <a class="transition-all hover-text-white" href="#">
+          <FaTwitter />
+        </a>
+        <a class="transition-all hover-text-white" href="#">
+          <FaFacebook />
+        </a>
+      </div>
+    </div>
+
+    <div class="py-10 border-t border-t-white/10">
+      <div class="container mx-auto">
+        <div class="text-sm text-center text-white/60">
+          Copyright &copy; Photolab 2024. All rights reserved.
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import FiMenu from "@/components/ui/icons/FiMenu.vue";
+import SlBag from "@/components/ui/icons/SlBag.vue";
+import { useCart } from "@/store/cart";
+
+const { setIsOpen } = useCart();
+const { isOpen, totalItems } = storeToRefs(useCart());
+
+const catNavMobile = ref(false);
+</script>
+
+<template>
+  <header class="fixed top-0 z-40 py-6 w-full bg-primary">
+    <div class="container mx-auto">
+      <div
+        class="flex flex-row gap-5 justify-between mb-4 lg:items-center lg:mb-0"
+      >
+        <div
+          class="text-3xl cursor-pointer xl:hidden"
+          @click="catNavMobile = true"
+        >
+          <FiMenu />
+        </div>
+
+        <div
+          :class="`${
+            catNavMobile ? 'left-0' : '-left-full'
+          } fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`"
+        >
+          <CategoryNavMobile @close="catNavMobile = false" />
+        </div>
+
+        <NuxtLink to="/">
+          <div class="text-3xl font-semibold">
+            PHOTO<span class="text-accent">LAB</span>
+          </div>
+        </NuxtLink>
+
+        <div class="hidden w-full lg:flex lg:max-w-[734px]">
+          <SearchForm />
+        </div>
+
+        <div class="flex items-center gap-x-[10px]">
+          <div class="hidden uppercase xl:flex">Need help? 123 456 789</div>
+
+          <div class="relative cursor-pointer" @click="setIsOpen(!isOpen)">
+            <SlBag class="text-2xl" />
+
+            <div
+              class="bg-accent text-primary absolute min-w-[20px] min-h-[20px] rounded-full top-3 -right-1 text-[13px] flex justify-center items-center font-bold tracing-[-0.1em] px-[3px]"
+            >
+              {{ totalItems }}
+            </div>
+          </div>
+
+          <div
+            :class="`${
+              isOpen ? 'right-0' : '-right-full'
+            } bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`"
+          >
+            <ClientOnly>
+              <Cart />
+            </ClientOnly>
+          </div>
+        </div>
+      </div>
+
+      <div class="lg:hidden">
+        <SearchForm />
+      </div>
+    </div>
+  </header>
+</template>
