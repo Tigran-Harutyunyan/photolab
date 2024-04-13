166a83755b3811f77adbb1f3c5f13cd38d715d75
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:46:18 +0400

    created CategoryNav components

---
+++ components/CategoryNav.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useMain } from "@/store/main";
+
+const { categories } = storeToRefs(useMain());
+
+const currentCategoryId = computed(() => {
+  return useRoute().params?.categoryId;
+});
+</script>
+
+<template>
+  <aside class="hidden xl:flex h-[auto]">
+    <div
+      class="bg-primary flex flex-col w-[286px] rounded-[10px] overflow-hidden"
+    >
+      <div
+        class="flex justify-center items-center py-4 font-semibold uppercase bg-accent text-primary"
+      >
+        Browse Categories
+      </div>
+
+      <div class="flex flex-col gap-y-6 p-6">
+        <NuxtLink
+          v-for="category in categories"
+          :key="category.id"
+          class="uppercase cursor-pointer"
+          :class="{ 'text-accent': currentCategoryId === String(category.id) }"
+          :to="`/category/${category.id}`"
+        >
+          {{ category?.attributes?.title }}
+        </NuxtLink>
+      </div>
+    </div>
+  </aside>
+</template>
---
+++ components/CategoryNavMobile.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import FiX from "@/components/ui/icons/FIX.vue";
+const emit = defineEmits(["close"]);
+
+import { useMain } from "@/store/main";
+
+const { categories } = storeToRefs(useMain());
+
+const currentCategoryId = computed(() => {
+  return useRoute().params?.categoryId;
+});
+</script>
+
+<template>
+  <div class="p-8 w-full h-full bg-primary">
+    <div class="flex justify-end mb-8 cursor-pointer" @click="emit('close')">
+      <FiX class="text-3xl" />
+    </div>
+    <div class="flex flex-col gap-y-8">
+      <NuxtLink
+        v-for="category in categories"
+        :key="category.id"
+        class="font-medium uppercase"
+        :class="{ 'text-accent': currentCategoryId === String(category.id) }"
+        :to="`/category/${category.id}`"
+      >
+        {{ category?.attributes.title }} cameras
+      </NuxtLink>
+    </div>
+  </div>
+</template>
