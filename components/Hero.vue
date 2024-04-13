b10ee01c3bb769a947cefb59cccb93a6266a7738
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-13 12:47:13 +0400

    created Hero and Promo components

---
+++ components/Hero.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import MainSlider from "./MainSlider.vue";
+import Promo from "./Promo.vue";
+
+import { PromoImg1, PromoImg2 } from "../public/images";
+</script>
+
+<template>
+  <section class="mb-[30px]">
+    <div class="container mx-auto">
+      <div class="flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]">
+        <CategoryNav />
+
+        <div class="w-full max-w-lg lg:max-w-[734px] mx-auto">
+          <MainSlider />
+        </div>
+
+        <section
+          class="flex flex-col lg:flex-row xl:flex-col gap-y-[30px] gap-x-[30px] w-full mx-auto h-[auto] lg:max-w-[734px] sm:max-w-[512px]"
+        >
+          <Promo
+            :image="PromoImg1"
+            imageStyles="top-3 -right-1"
+            text="Save 35% all dslr cameras"
+          />
+
+          <Promo
+            :image="PromoImg2"
+            imageStyles="top-3 -right-4"
+            text="Save 25% mirrorless cameras"
+          />
+        </section>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ components/Promo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+type Props = {
+  text: string;
+  image: string;
+  imageStyles?: string;
+};
+const { text, image, imageStyles } = defineProps<Props>();
+</script>
+
+<template>
+  <section
+    class="grad flex-1 h-[250px] rounded-[10px] overflow-hidden relative p-6 min-h-[250px]"
+  >
+    <div class="flex flex-col max-w-[144px] h-full justify-center">
+      <div class="text-[20px] uppercase font-medium leading-tight mb-4">
+        {{ text }}
+      </div>
+      <NuxtLink class="uppercase text-accent" href="#"> Shop now </NuxtLink>
+    </div>
+
+    <img
+      alt="Promo image"
+      :class="`absolute z-20 ${imageStyles}`"
+      :src="image"
+    />
+  </section>
+</template>
