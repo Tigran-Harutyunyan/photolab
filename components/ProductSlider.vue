d3cb772030bb2010ecaab8ae0f737c42c385ef5e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-22 10:48:15 +0400

    improved slider

--- components/ProductSlider.vue
+++ components/ProductSlider.vue
@@ -6,38 +6,42 @@ interface Props {
 }
 const props = defineProps<Props>();
 const { products } = toRefs(props);
+const showSlider =ref(false);
 </script>
 
 <template>
   <Swiper
     v-if="products"
+    slidesPerView: 5, 
     :modules="[SwiperPagination, SwiperNavigation]"
     :loop="false"
     navigation
-    :pagination="{
-      clickable: true,
-    }"
+    @init="showSlider=true"
     :breakpoints="{
       320: {
         slidesPerView: 1,
+        slidesPerGroup: 1,
         spaceBetween: 30,
       },
       768: {
         slidesPerView: 2,
+        slidesPerGroup: 2,
         spaceBetween: 30,
       },
       1024: {
         slidesPerView: 2,
+        slidesPerGroup: 2,
         spaceBetween: 30,
       },
       1440: {
         slidesPerView: 5,
+        slidesPerGroup: 5,
         spaceBetween: 30,
       },
     }"
     class="productSlider mx-auto sm:max-w-[512px] lg:max-w-[734px] xl:max-w-[1410px]"
   >
-    <SwiperSlide v-for="item in products" :key="item._id">
+    <SwiperSlide v-for="item in products" :key="item._id" v-show=showSlider>
       <Product :product="item" />
     </SwiperSlide>
   </Swiper>
